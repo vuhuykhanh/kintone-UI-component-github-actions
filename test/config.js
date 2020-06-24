111936f25edc6d7f05935b81e9402879613609e8
Vu Huy Khanh <k001534@cb.local>
2020-06-24 10:47:04 +0700

    Up

--- test/config.js
+++ test/config.js
@@ -2,8 +2,8 @@ module.exports = {
     "domain": "khanhvu-2.cybozu-dev.com",	 
     "appId": 3,	  
     "credentials": {
-        "username": cybozu,	
-        "password": cybozu
+        "username": "cybozu",	
+        "password": "cybozu"
     },	  
 	"folderTest": js
 }
